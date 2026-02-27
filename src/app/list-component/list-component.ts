fa6e221240e34cb5205bac3ef33910f58a80af69
LiamAtu <g00435609@atu.ie>
2026-02-27 13:28:46 +0000

    added comments

--- src/app/list-component/list-component.ts
+++ src/app/list-component/list-component.ts
@@ -10,4 +10,12 @@ import { Component } from '@angular/core';
 export class ListComponent {
   //VARIABLES
   presidents: string[] = ["Higgins", "McAleese", "Robinson", "Hillery"];
+
+  //THIS IS AN  ANY ARRAY THAT HOLDS NAME AND TERM OF PRESIDENTS
+  myPresidents: any[] =
+[{name: "Higgins", term:"2011-"},
+{name: "McAleese", term:"1997-2011"},
+{name: "Robinson", term:"1990-1997"},
+{name: "Hillery", term:"1976-1990"},
+{name: "O Dalaigh", term:"1974-1976"}];
 }
