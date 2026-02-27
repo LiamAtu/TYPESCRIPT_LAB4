c9d26ac156aa60485bb709358445db3d51a60662
LiamAtu <g00435609@atu.ie>
2026-02-27 13:20:16 +0000

    TYPESCRIPT-LAB4

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,36 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListComponent } from './list-component/list-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ListComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('data-binding-app');
+  
+  //VARIABLES
+  name:string="Liam";
+  count:number =0;
+  myBool:boolean = true;
+  
+  //METHOD THAT INCREMENTS COUNTER ON BUTTON CLICK
+  onButtonClicked()
+  {
+    this.count++;
+  }
+  //METHOD THAT SHOWS AND HIDES H2 ELEMENT ON DBL CLICK
+  onDoubleClicked()
+  {
+    if(!this.myBool)
+    {
+      this.myBool=true;
+    }
+    else
+    {
+      this.myBool=false;
+    }
+      
+  }
 }
